9ffc01dc21db298677282a76d88dbd3f6c594bb2
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-03 21:53:41 +0400

    Added Window Transparency API
    To Make Mouse click through

--- API/User32API.cs
+++ API/User32API.cs
@@ -3,10 +3,11 @@ using System.Runtime.InteropServices;
 
 namespace SequenceClicker.API
 {
-    //Based on https://github.com/oriash93/AutoClicker User32ApiUtils.cs
+    //Based on https://github.com/oriash93/AutoClicker User32ApiUtils.cs and https://social.msdn.microsoft.com/Forums/en-US/a3cb7db6-5014-430f-a5c2-c9746b077d4f/click-through-windows-and-child-image-issue?forum=wpf
 
     public static class User32API
     {
+        #region Mouse Events
         [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
         internal static extern bool SetCursorPosition(int x, int y);
 
@@ -18,5 +19,26 @@ namespace SequenceClicker.API
 
         [DllImport("user32.dll")]
         internal static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        #endregion
+
+        #region Window Click Through
+        public const int WS_EX_TRANSPARENT = 0x00000020;
+        public const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetWindowTransparent(IntPtr hwnd, bool state)
+        {
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+
+            int newStyleTag = state ? extendedStyle | WS_EX_TRANSPARENT : extendedStyle & ~WS_EX_TRANSPARENT;
+
+            User32API.SetWindowLong(hwnd, GWL_EXSTYLE, newStyleTag);
+        }
+        #endregion
     }
 }
