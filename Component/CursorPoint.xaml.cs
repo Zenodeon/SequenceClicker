3b66c2743fddd930ad762ddd891c961db4b802eb
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-03 22:46:38 +0400

    CursorPoint UserControl

---
+++ Component/CursorPoint.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SequenceClicker.Component
+{
+    /// <summary>
+    /// Interaction logic for CursorPoint.xaml
+    /// </summary>
+    public partial class CursorPoint : UserControl
+    {
+        public CursorPoint()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
