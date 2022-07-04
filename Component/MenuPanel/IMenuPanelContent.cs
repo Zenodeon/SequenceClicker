2d5511e9a3d9af1a6f43867ed383c22d26e1711d
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 23:31:07 +0400

    Create IMenuPanelContent.cs

---
+++ Component/MenuPanel/IMenuPanelContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceClicker.Component
+{
+    public interface IMenuPanelContent
+    {
+        void AddMenuContent(ref List<MenuButtonBP> buttons);
+    }
+}
