bd0d7515f7cc85ce6be3acf9982a642ef9e6f313
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 23:31:15 +0400

    Create MenuButtonBP.cs

---
+++ Component/MenuPanel/MenuButtonBP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceClicker.Component
+{
+    public struct MenuButtonBP
+    {
+        public string name { get; set; }
+        public Action callback { get; set; }
+
+        public MenuButtonBP(string name, Action callback)
+        {
+            this.name = name;
+            this.callback = callback;
+        }
+    }
+}
