517794971bd6816e4adfa65c1a82bac9e9f63cd3
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 20:41:12 +0400

    MenuPanel

---
+++ Component/MenuPanel/MenuPanel.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SequenceClicker.Component
+{
+    /// <summary>
+    /// Interaction logic for MenuPanel.xaml
+    /// </summary>
+    public partial class MenuPanel : UserControl
+    {
+        public bool active { get; set; } = false;
+
+        public MenuPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            LocalState.overlayWindow.CloseMenuPanel();
+        }
+    }
+}
