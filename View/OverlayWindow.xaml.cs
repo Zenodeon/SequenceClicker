4ff2763d20ac2c2e248284012bec436b2f9ecd1f
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 20:41:30 +0400

    Menu Panel and Multiple Point Test

--- View/OverlayWindow.xaml.cs
+++ View/OverlayWindow.xaml.cs
@@ -24,14 +24,40 @@ namespace SequenceClicker.View
         {
             InitializeComponent();
 
-            CursorPoint cursorPoint = new CursorPoint();
+            LocalState.menuPanel = new MenuPanel();
+
+            CursorPoint cursorPoint = new CursorPoint(0);
+            CursorPoint cursorPoint1 = new CursorPoint(1);
 
             CursorSite.Children.Add(cursorPoint);
-            cursorPoint.Visibility = Visibility.Visible;
+            CursorSite.Children.Add(cursorPoint1);
 
             Canvas.SetLeft(cursorPoint, 100);
             Canvas.SetTop(cursorPoint, 100);
             Canvas.SetZIndex(cursorPoint, 0);
+            Canvas.SetZIndex(cursorPoint1, -1);
+        }
+
+        public void OpenMenuPanel(Point screenPos, CursorPoint cursorPoint)
+        {
+            MenuPanel panel = LocalState.menuPanel;
+
+            if (!panel.active)
+            {
+                CursorSite.Children.Add(panel);
+                panel.active = true;
+            }
+
+            Canvas.SetLeft(panel, screenPos.X);
+            Canvas.SetTop(panel, screenPos.Y);
+            Canvas.SetZIndex(panel, 1);
+        }
+
+        public void CloseMenuPanel()
+        {
+            MenuPanel panel = LocalState.menuPanel;
+            CursorSite.Children.Remove(panel);
+            panel.active = false;
         }
     }
 }
