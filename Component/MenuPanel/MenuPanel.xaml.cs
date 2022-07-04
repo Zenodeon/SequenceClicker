2612dc3ed3e306012ca3c7c2ae36b10578820f32
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 23:31:46 +0400

    Menu panel with menu buttons

--- Component/MenuPanel/MenuPanel.xaml.cs
+++ Component/MenuPanel/MenuPanel.xaml.cs
@@ -22,14 +22,47 @@ namespace SequenceClicker.Component
     {
         public bool active { get; set; } = false;
 
+        List<MenuButton> buttons = new List<MenuButton>();
+
         public MenuPanel()
         {
             InitializeComponent();
+            MBControl.ItemsSource = buttons;
         }
 
         private void OnMouseLeave(object sender, MouseEventArgs e)
         {
             LocalState.overlayWindow.CloseMenuPanel();
+        }
+
+        public void OpenPanel(IMenuPanelContent panelContent)
+        {
+            active = true;
+
+            List<MenuButtonBP> buttonBPs = new List<MenuButtonBP>();
+            panelContent.AddMenuContent(ref buttonBPs);
+
+            ConstructMenu(buttonBPs);
+        }
+
+        public void ClosePanel()
+        {
+            active = false;
+            buttons.Clear();
+        }
+
+        private void ConstructMenu(List<MenuButtonBP> buttonBPs)
+        {
+            int bpCount = buttonBPs.Count;
+            for (int i = 0; i < bpCount; i++)
+            {
+                MenuButton newButton = new MenuButton(buttonBPs[i]);
+                buttons.Add(newButton);
+
+                if (i == 0)
+                    this.Height = newButton.Height * bpCount;
+            }
+
         }
     }
 }
