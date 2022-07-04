591b4cfdcecfb5ff8f1b3667e89438c08945fadf
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 13:01:48 +0400

    Failed Hotkey attempt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace SequenceClicker
 
         bool test = false;
 
+        private IntPtr hwnd;
+        private HwndSource? hwndSource;
+
         public MainWindow()
         {
             DLog.Instantiate();
@@ -37,11 +40,49 @@ namespace SequenceClicker
             overlayWindow.Show();
         }
 
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            hwnd = new WindowInteropHelper(this).Handle;
+            //hwndSource = HwndSource.FromHwnd(hwnd);
+            //hwndSource.AddHook(MsgHook);
+
+            //User32API.RegisterHotKey(hwnd, 1, User32DS.HKMod.MOD_CONTROL, Key.P);
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             test = !test;
             IntPtr hwnd = new WindowInteropHelper(overlayWindow).Handle;
             User32API.SetWindowTransparent(hwnd, test);
+        }
+
+        private IntPtr MsgHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            string hotkeyId = wParam.ToString();
+
+            if (msg == User32DS.WM_HOTKEY)
+            {
+                DLog.Log(msg + " : " + hotkeyId);
+                handled = true;
+            }
+            //if (msg == Constants.WM_HOTKEY && hotkeyId == Constants.START_HOTKEY_ID || hotkeyId == Constants.STOP_HOTKEY_ID || hotkeyId == Constants.TOGGLE_HOTKEY_ID)
+            //{
+            //    int virtualKey = ((int)lParam >> 16) & 0xFFFF;
+            //    if (virtualKey == SettingsUtils.CurrentSettings.HotkeySettings.StartHotkey.VirtualKeyCode && CanStartOperation())
+            //    {
+            //        StartCommand_Execute(null, null);
+            //    }
+            //    if (virtualKey == SettingsUtils.CurrentSettings.HotkeySettings.StopHotkey.VirtualKeyCode && clickTimer.Enabled)
+            //    {
+            //        StopCommand_Execute(null, null);
+            //    }
+            //    if (virtualKey == SettingsUtils.CurrentSettings.HotkeySettings.ToggleHotkey.VirtualKeyCode && CanStartOperation() | clickTimer.Enabled)
+            //    {
+            //        ToggleCommand_Execute(null, null);
+            //    }
+            //    handled = true;
+            //}
+            return IntPtr.Zero;
         }
     }
 }
