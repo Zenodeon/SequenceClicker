acec99b03080e739d122bfb818013afa5dfc2c63
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-04 20:39:17 +0400

    LocalState to store common reference

---
+++ LocalState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SequenceClicker.Component;
+using SequenceClicker.View;
+
+namespace SequenceClicker
+{
+    public static class LocalState
+    {
+        public static MainWindow mainWindow { get; set; }
+        public static OverlayWindow overlayWindow { get; set; }
+
+        public static MenuPanel menuPanel { get; set; }
+    }
+}
