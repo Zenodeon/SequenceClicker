501bba47bdf2d524045e2517e6fe3bf6d74253a1
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-05 22:31:09 +0400

    Create CanvasUtility.cs

---
+++ Utility/CanvasUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SequenceClicker
+{
+    public static class CanvasUtility
+    {
+
+        public static void SetCanvasPosition(this UIElement element, double x, double y, int z)
+        {
+            element.SetCanvasPosition(x, y);
+            Canvas.SetZIndex(element, z);
+        }
+        public static void SetCanvasPosition(this UIElement element, double x, double y)
+        {
+            Canvas.SetLeft(element, x);
+            Canvas.SetTop(element, y);
+        }
+        public static void SetCanvasPosition(this UIElement element, Point position, int z)
+            => element.SetCanvasPosition(position.X, position.Y, z);
+
+        public static void SetCanvasPosition(this UIElement element, Point position)
+            => element.SetCanvasPosition(position.X, position.Y);
+    }
+}
