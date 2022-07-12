095bc8b37652fad6992f72d20143450aa2afa342
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-13 00:00:30 +0400

    Sequencer UserControl

---
+++ View/Sequencer/Sequencer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SequenceClicker.View
+{
+    /// <summary>
+    /// Interaction logic for Sequencer.xaml
+    /// </summary>
+    public partial class Sequencer : UserControl
+    {
+        public Sequencer()
+        {
+            InitializeComponent();
+        }
+    }
+}
