80e097d2e012fe2a69e765c0ec443e987d354040
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-20 18:54:33 +0400

    Switch Between Static and Random Mode

--- View/BasicSequencer/Component/DelayControl.xaml.cs
+++ View/BasicSequencer/Component/DelayControl.xaml.cs
@@ -21,9 +21,40 @@ namespace SequenceClicker.View.BasicSequencer.Component
     /// </summary>
     public partial class DelayControl : UserControl
     {
+        public DelayMode delayMode = DelayMode.Random;
+
         public DelayControl()
         {
             InitializeComponent();
+        }
+
+        private void OnDelayModeSwitch(object sender, RoutedEventArgs e)
+        {
+            if (delayMode == DelayMode.Static)
+                delayMode = DelayMode.Random;
+            else
+                delayMode = DelayMode.Static;
+
+            SwitchDelayInput(delayMode);
+
+            DelayModeSwitch.Text = delayMode.ToString() + " Delay";
+        }
+
+        private void SwitchDelayInput(DelayMode delayMode)
+        {
+            bool isStaticMode = delayMode == DelayMode.Static;
+            bool isRandomMode = delayMode == DelayMode.Random;
+
+            SInputCtrl.Visibility = isStaticMode? Visibility.Visible : Visibility.Collapsed;
+
+            RInputCtrl1.Visibility = isRandomMode ? Visibility.Visible : Visibility.Collapsed;
+            RInputCtrl2.Visibility = isRandomMode ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public enum DelayMode
+        {
+            Static,
+            Random
         }
     }
 }
