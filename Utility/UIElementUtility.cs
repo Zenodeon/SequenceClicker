5c37c2434d5352f66cfe618747da64f849bea67a
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-08-12 00:04:57 +0530

    Simple

--- Utility/UIElementUtility.cs
+++ Utility/UIElementUtility.cs
@@ -10,10 +10,8 @@ namespace SequenceClicker
 {
     public static class UIElementUtility
     {
-        public static ElementPropertyHandler FadeProperty(this UIElement element, DependencyProperty dp, double value, int msDuration = 150)
+        public static DoubleAnimation FadeProperty(this UIElement element, DependencyProperty dp, double value, int msDuration = 150)
         {
-            ElementPropertyHandler handler = new ElementPropertyHandler();
-
             DoubleAnimation animation = new DoubleAnimation
             {
                 From = (double)element.GetValue(dp),
@@ -22,37 +20,23 @@ namespace SequenceClicker
                 FillBehavior = FillBehavior.Stop
             };
 
-            animation.Completed += (o, e) => element.SetValue(dp, value);
+            animation.OnComplete(() => element.SetValue(dp, value));
            
             element.BeginAnimation(dp, animation);
 
-            handler.animation = animation;
-            return handler;
+            return animation;
         }
 
-        public static ElementPropertyHandler OnComplete(this ElementPropertyHandler handler, Action action)
+        public static DoubleAnimation OnComplete(this DoubleAnimation animation, Action action)
         {
-            handler.completeAction = action;
-            handler.animation.Completed += handler.OnComplete;
-            return handler;
+            animation.Completed += (o, e) => action();
+            return animation;
         }
 
-        public static ElementPropertyHandler OnUpdate(this ElementPropertyHandler handler, Action action)
+        public static DoubleAnimation OnUpdate(this DoubleAnimation animation, Action action)
         {
-            handler.updateAction = action;
-            handler.animation.CurrentTimeInvalidated += handler.OnUpdate;
-            return handler;
-        }
-
-        public class ElementPropertyHandler
-        {
-            public DoubleAnimation animation;
-
-            public Action updateAction;
-            public Action completeAction;
-
-            public void OnUpdate(object? obj, EventArgs e) => updateAction();
-            public void OnComplete(object? obj, EventArgs e) => completeAction();
+            animation.CurrentTimeInvalidated += (o, e) => action();
+            return animation;
         }
     }
 }
