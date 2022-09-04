436df4c56b0f541d87586585d568d7b44797e7b3
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-09-04 18:54:45 +0400

    Create UUtility.cs

---
+++ Utility/UUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceClicker
+{
+    public static class UUtility
+    {
+        public static float RangedMapClamp(float value, float InMinimum, float InMaximum, float OutMinimum, float OutMaximum)
+        {
+            var InRange = InMaximum - InMinimum;
+            var OutRange = OutMaximum - OutMinimum;
+            var finalValue = ((value - InMinimum) * OutRange / InRange) + OutMinimum;
+
+            return finalValue;
+        }
+    }
+}
